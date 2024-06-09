113a9e0ed0a8c5106ed5f64133eaf6495cf66733
Vitor Nunes Silva <vitornunessil17@gmail.com>
2024-06-09 15:16:54 -0300

    Alterações na pág CadastroVendas

--- TCC/public/js/index.js
+++ TCC/public/js/index.js
@@ -21,20 +21,20 @@ dropdownButtons.forEach(function (button) {
 });
 
 // Função para formatar o número de telefone conforme o usuário digita
-// document.getElementById('cpf').addEventListener('input', function (e) {
-//     var cpf = e.target.value.replace(/\D/g, ''); 
-//     cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2'); 
-//     cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2'); 
-//     cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2'); 
-//     e.target.value = cpf;
-// });
+document.getElementById('cpf').addEventListener('input', function (e) {
+    var cpf = e.target.value.replace(/\D/g, ''); 
+    cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2'); 
+     cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2'); 
+     cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2'); 
+     e.target.value = cpf;
+ });
 
-// document.getElementById('tel').addEventListener('input', function (e) {
-//     var tel = e.target.value.replace(/\D/g, ''); 
-//     tel = tel.replace(/(\d{2})(\d)/, '($1) $2'); 
-//     tel = tel.replace(/(\d{5})(\d)/, '$1-$2');
-//     e.target.value = tel;
-// });
+ document.getElementById('tel').addEventListener('input', function (e) {
+     var tel = e.target.value.replace(/\D/g, ''); 
+     tel = tel.replace(/(\d{2})(\d)/, '($1) $2'); 
+     tel = tel.replace(/(\d{5})(\d)/, '$1-$2');
+     e.target.value = tel;
+ });
 
 // Função para remover formatação antes de enviar para o banco de dados
 function enviarParaBancoDeDados() {
@@ -43,11 +43,11 @@ function enviarParaBancoDeDados() {
     console.log("Telefone sem formatação:", telefoneSemFormatacao);
 }
 
-// document.getElementById('cep').addEventListener('input', function (e) {
-//     var cep = e.target.value.replace(/\D/g, ''); 
-//     cep = cep.replace(/^(\d{5})(\d)/, '$1-$2'); 
-//     e.target.value = cep;
-// });
+ document.getElementById('cep').addEventListener('input', function (e) {
+     var cep = e.target.value.replace(/\D/g, ''); 
+     cep = cep.replace(/^(\d{5})(\d)/, '$1-$2'); 
+     e.target.value = cep;
+ });
 
 // Seleciona o elemento da imagem do logo
 var trocarLogo = document.getElementById('trocarLogo');
@@ -71,49 +71,3 @@ function loadHeaderAndSidebar() {
     loadComponent('components.html', 'components');
 }
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     var toggleSidebarButton = document.getElementById('toggleSidebar');
-//     var sidebar = document.getElementById('sidebar01');
-
-//     toggleSidebarButton.addEventListener('click', function () {
-//         sidebar.classList.toggle('hidden-sidebar');
-//     });
-// });
-
-document.getElementById('addRowButton').addEventListener('click', function() {
-    var table = document.getElementById('vendasTable').getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow();
-
-    var cells = ['produto', 'detalhes', 'quantidade', 'valor', 'subtotal'];
-    cells.forEach(function(cell) {
-        var newCell = newRow.insertCell();
-        var input;
-        if (cell === 'detalhes') {
-            input = document.createElement('textarea');
-            input.id = cell;
-            input.name = cell;
-            input.required = true;
-            input.style.height = '30px';  // Match the height of other inputs
-        } else {
-            input = document.createElement('input');
-            input.type = cell === 'quantidade' || cell === 'valor' || cell === 'subtotal' ? 'number' : 'text';
-            input.id = cell;
-            input.name = cell;
-            input.required = cell !== 'desconto';
-            input.step = cell === 'valor' || cell === 'subtotal' ? '0.01' : undefined;
-        }
-        input.style.width = '100%';
-        input.style.height = '30px';
-        input.style.borderRadius = '5px';
-        input.style.border = '1px solid rgb(211, 205, 205)';
-        input.style.boxSizing = 'border-box';
-        newCell.appendChild(input);
-    });
-
-    // Adiciona o botão de cadastrar na nova linha
-    var actionCell = newRow.insertCell();
-    var submitButton = document.createElement('button');
-    submitButton.type = 'submit';
-    submitButton.innerText = 'CADASTRAR';
-    actionCell.appendChild(submitButton);
-});
--- TCC/resources/views/cadastrovendas.blade.php
+++ TCC/resources/views/cadastrovendas.blade.php
@@ -19,70 +19,6 @@
         </div>
     </div>
     <br>
-    <!-- <div class="cadastro">
-        <div style="display: flex;">
-            <img src="{{ asset('/img/edit.png') }}">
-            <h5>Cadastro de Vendas</h5>
-        </div>
-        <hr>
-        <div class="forms">
-            <form id="cadastrarVendas">
-            @csrf
-                <div class="table_form">
-                    <label class="obg" for="cliente_id">Cliente</label>
-                    <input type="text" id="cliente_id" name="cliente_id" required>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="produtos_id">Produtos</label>
-                    <input type="text" id="produtos_id" name="produtos_id" required>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="situacao">Situação</label>
-                    <input type="text" id="situacao" name="situacao" required>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="dataEntregaMercadoria">Data de Entrega da Mercadoria</label>
-                    <input type="date" id="dataEntregaMercadoria" name="dataEntregaMercadoria" required>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="dataRecebimento">Data de Recebimento</label>
-                    <input type="date" id="dataRecebimento" name="dataRecebimento" required>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="detalhes">Detalhes</label>
-                    <textarea id="detalhes" name="detalhes" required></textarea>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="quantidade">Quantidade</label>
-                    <input type="number" id="quantidade" name="quantidade" required>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="valor">Valor</label>
-                    <input type="number" id="valor" name="valor" step="0.01" required>
-                </div>
-                <div class="table_form">
-                    <label for="desconto">Desconto</label>
-                    <input type="number" id="desconto" name="desconto" step="0.01">
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="subtotal">Subtotal</label>
-                    <input type="number" id="subtotal" name="subtotal" step="0.01" required>
-                </div>
-                <div class="table_form">
-                    <label for="observacoes">Observações</label>
-                    <textarea id="observacoes" name="observacoes"></textarea>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="valorTotal">Valor Total</label>
-                    <input type="number" id="valorTotal" name="valorTotal" step="0.01" required>
-                </div>
-                <div class="button-container">
-                    <button type="submit">CADASTRAR</button>
-                </div>
-            </form>
-        </div>
-    </div> -->
-
     <div class="cadastro">
         <div style="display: flex;">
             <img src="{{ asset('/img/edit.png') }}">
@@ -90,78 +26,75 @@
         </div>
         <hr>
         <div class="forms">
-            <form id="cadastrarVendas">
-                @csrf
-                <div class="table_form">
-                    <label class="obg" for="cliente_nome">Cliente</label>
-                    <input type="text" id="cliente_nome" name="cliente_nome" required>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="endereco_rua">Rua</label>
-                    <input type="text" id="endereco_rua" name="endereco_rua" required>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="endereco_numero">Número</label>
-                    <input type="text" id="endereco_numero" name="endereco_numero" required>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="endereco_bairro">Bairro</label>
-                    <input type="text" id="endereco_bairro" name="endereco_bairro" required>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="endereco_cidade">Cidade</label>
-                    <input type="text" id="endereco_cidade" name="endereco_cidade" required>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="endereco_estado">Estado</label>
-                    <select id="endereco_estado" name="endereco_estado" required>
-                        <option value="" disabled selected>Selecione o estado</option>
-                        <option value="AC">Acre</option>
-                        <option value="AL">Alagoas</option>
-                        <option value="AP">Amapá</option>
-                        <option value="AM">Amazonas</option>
-                        <option value="BA">Bahia</option>
-                        <option value="CE">Ceará</option>
-                        <option value="DF">Distrito Federal</option>
-                        <option value="ES">Espírito Santo</option>
-                        <option value="GO">Goiás</option>
-                        <option value="MA">Maranhão</option>
-                        <option value="MT">Mato Grosso</option>
-                        <option value="MS">Mato Grosso do Sul</option>
-                        <option value="MG">Minas Gerais</option>
-                        <option value="PA">Pará</option>
-                        <option value="PB">Paraíba</option>
-                        <option value="PR">Paraná</option>
-                        <option value="PE">Pernambuco</option>
-                        <option value="PI">Piauí</option>
-                        <option value="RJ">Rio de Janeiro</option>
-                        <option value="RN">Rio Grande do Norte</option>
-                        <option value="RS">Rio Grande do Sul</option>
-                        <option value="RO">Rondônia</option>
-                        <option value="RR">Roraima</option>
-                        <option value="SC">Santa Catarina</option>
-                        <option value="SP">São Paulo</option>
-                        <option value="SE">Sergipe</option>
-                        <option value="TO">Tocantins</option>
-                    </select>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="data_entrega">Data de Entrega</label>
-                    <input type="date" id="data_entrega" name="data_entrega" required>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="data_retorno">Data de Retorno</label>
-                    <input type="date" id="data_recebimento" name="data_recebimento" required>
-                </div>
-                <div class="table_form">
-                    <label class="obg" for="situacao">Situação</label>
-                    <select id="situacao" name="situacao" required>
-                        <option value="paga">Paga</option>
-                        <option value="em_aberto">Em Aberto</option>
-                        <option value="a_pagar">A Pagar</option>
-                    </select>
-                </div>
-            </form>
+            <div class="table_form">
+                <label class="obg" for="cliente_nome">Cliente</label>
+                <input type="text" id="cliente_nome" name="cliente_nome" required>
+            </div>
+            <div class="table_form">
+                <label class="obg" for="endereco_rua">Rua</label>
+                <input type="text" id="endereco_rua" name="endereco_rua" required>
+            </div>
+            <div class="table_form">
+                <label class="obg" for="endereco_numero">Número</label>
+                <input type="text" id="endereco_numero" name="endereco_numero" required>
+            </div>
+            <div class="table_form">
+                <label class="obg" for="endereco_bairro">Bairro</label>
+                <input type="text" id="endereco_bairro" name="endereco_bairro" required>
+            </div>
+            <div class="table_form">
+                <label class="obg" for="endereco_cidade">Cidade</label>
+                <input type="text" id="endereco_cidade" name="endereco_cidade" required>
+            </div>
+            <div class="table_form">
+                <label class="obg" for="endereco_estado">Estado</label>
+                <select id="endereco_estado" name="endereco_estado" required>
+                    <option value="" disabled selected>Selecione o estado</option>
+                    <option value="AC">Acre</option>
+                    <option value="AL">Alagoas</option>
+                    <option value="AP">Amapá</option>
+                    <option value="AM">Amazonas</option>
+                    <option value="BA">Bahia</option>
+                    <option value="CE">Ceará</option>
+                    <option value="DF">Distrito Federal</option>
+                    <option value="ES">Espírito Santo</option>
+                    <option value="GO">Goiás</option>
+                    <option value="MA">Maranhão</option>
+                    <option value="MT">Mato Grosso</option>
+                    <option value="MS">Mato Grosso do Sul</option>
+                    <option value="MG">Minas Gerais</option>
+                    <option value="PA">Pará</option>
+                    <option value="PB">Paraíba</option>
+                    <option value="PR">Paraná</option>
+                    <option value="PE">Pernambuco</option>
+                    <option value="PI">Piauí</option>
+                    <option value="RJ">Rio de Janeiro</option>
+                    <option value="RN">Rio Grande do Norte</option>
+                    <option value="RS">Rio Grande do Sul</option>
+                    <option value="RO">Rondônia</option>
+                    <option value="RR">Roraima</option>
+                    <option value="SC">Santa Catarina</option>
+                    <option value="SP">São Paulo</option>
+                    <option value="SE">Sergipe</option>
+                    <option value="TO">Tocantins</option>
+                </select>
+            </div>
+            <div class="table_form">
+                <label class="obg" for="data_entrega">Data de Entrega</label>
+                <input type="date" id="data_entrega" name="data_entrega" required>
+            </div>
+            <div class="table_form">
+                <label class="obg" for="data_retorno">Data de Retorno</label>
+                <input type="date" id="data_recebimento" name="data_recebimento" required>
+            </div>
+            <div class="table_form">
+                <label class="obg" for="situacao">Situação</label>
+                <select id="situacao" name="situacao" required>
+                    <option value="paga">Paga</option>
+                    <option value="em_aberto">Em Aberto</option>
+                    <option value="a_pagar">A Pagar</option>
+                </select>
+            </div>
         </div>
     </div>
 
@@ -175,10 +108,10 @@
             <table>
                 <thead>
                     <tr>
-                        <th>Produto</th>
+                        <th>Produto<span style="color: red;"> *</span></th>
                         <th>Detalhes</th>
-                        <th>Quantidade</th>
-                        <th>Valor</th>
+                        <th>Quantidade<span style="color: red;"> *</span></th>
+                        <th>Valor<span style="color: red;"> *</span></th>
                         <th>Subtotal</th>
                         <th>Ações</th>
                     </tr>
@@ -201,12 +134,119 @@
                             <input type="number" id="subtotal" name="subtotal" step="0.01" required>
                         </td>
                         <td>
-                            <img src="{{ asset('/img/x.png') }}" style="max-width: 20px;">
+                            <a href="">
+                                <img src="{{ asset('/img/x.png') }}" style="max-width: 25px;">
+                            </a>
                         </td>
                     </tr>
                 </tbody>
             </table>
-            <button id="addRowButton" class="add-row-button">+ Adicionar</button>
+            <button id="addRowButton" class="add-row-button">
+                <img src="{{ asset('/img/plus.png') }}" alt="Mais">
+                Adicionar
+            </button>
         </div>
     </div>
+    <div class="cadastro">
+        <div style="display: flex;">
+            <img src="{{ asset('/img/dindin.png') }}">
+            <h5>Total</h5>
+        </div>
+        <hr>
+        <div class="forms">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Produtos<span style="color: red;"> *</span></th>
+                        <th>Serviços<span style="color: red;"> *</span></th>
+                        <th>Descontos<span style="color: red;"> *</span></th>
+                        <th>Valor Total<span style="color: red;"> *</span></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="number" id="produtos" name="produtos" required>
+                        </td>
+                        <td>
+                            <input type="number" id="servicos" name="servicos" required>
+                        </td>
+                        <td>
+                            <input type="number" id="descontos" name="descontos" step="0.01" required>
+                        </td>
+                        <td>
+                            <input type="number" id="valorTotal" name="valorTotal" step="0.01" required>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="cadastro">
+        <div style="display: flex;">
+            <img src="{{ asset('/img/dindin.png') }}">
+            <h5>Observações</h5>
+        </div>
+        <hr>
+        <p><i>Essa observação será impressa no recibo.</i></p>
+        <form>
+            <textarea id="observacoes" style="width: 50%;" name="observacoes" rows="4" cols="50" required></textarea>
+        </form>
+    </div>
+    <div>
+        <a>
+            <button class="botao_endpage" style="background-color: green; color: white; margin-left: 20px;">CADASTRAR</button>
+        </a>
+        <a>
+            <button class="botao_endpage" style="background-color: red; color: white;">CANCELAR</button>
+        </a>
+    </div>
+    
+    <script>
+        document.getElementById('addRowButton').addEventListener('click', function() {
+            var table = document.querySelector('table tbody');
+            var newRow = table.insertRow();
+
+            var cells = ['produto', 'detalhes', 'quantidade', 'valor', 'subtotal'];
+            cells.forEach(function(cell) {
+                var newCell = newRow.insertCell();
+                var input;
+                if (cell === 'detalhes') {
+                    input = document.createElement('textarea');
+                    input.id = cell;
+                    input.name = cell;
+                    input.required = true;
+                } else {
+                    input = document.createElement('input');
+                    input.type = cell === 'quantidade' || cell === 'valor' || cell === 'subtotal' ? 'number' : 'text';
+                    input.id = cell;
+                    input.name = cell;
+                    input.required = true;
+                    if (cell === 'valor' || cell === 'subtotal') {
+                        input.step = '0.01';
+                    }
+                }
+                input.style.width = '100%';
+                input.style.height = '30px';
+                input.style.borderRadius = '5px';
+                input.style.border = '1px solid rgb(211, 205, 205)';
+                input.style.boxSizing = 'border-box';
+                newCell.appendChild(input);
+            });
+
+            // Adiciona o botão de remover na nova linha
+            var actionCell = newRow.insertCell();
+            var removeButton = document.createElement('a');
+            var removeImg = document.createElement('img');
+            removeImg.src = '{{ asset('/img/x.png') }}';
+            removeImg.style.maxWidth = '25px';
+            removeButton.appendChild(removeImg);
+            removeButton.href = '#';
+            removeButton.addEventListener('click', function(event) {
+                event.preventDefault();
+                table.removeChild(newRow);
+            });
+            actionCell.appendChild(removeButton);
+        });
+    </script>
 @endsection
