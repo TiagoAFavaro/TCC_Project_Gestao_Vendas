9ef77402ad327705e253e8cc1c93da595ddc0205
Tiago Favaro <tiago.favaro@hotmail.com>
2024-06-25 23:32:51 -0300

    Rotas de update e get para visualizar Contas a pagar

--- TCC/app/Http/Controllers/CadastroContasaPagarController.php
+++ TCC/app/Http/Controllers/CadastroContasaPagarController.php
@@ -15,6 +15,13 @@ class CadastroContasaPagarController extends Controller
         return view('contasAPagar')->with('cadastros', $cadastros);
     }
 
+    public function visualizarContasPagarById($id)
+    {
+        $aPagar = CadastroContasaPagar::findOrFail($id);
+
+        return view('visualizarPagar',  ['aPagar' => $aPagar]);
+    }
+
     public function store(Request $request)
     {
         $cadastro = new CadastroContasaPagar();
@@ -31,5 +38,28 @@ class CadastroContasaPagarController extends Controller
         $cadastro->save();
 
         return redirect('/contas_a_pagar/list');
+    }
+
+    public function edit($id)
+    {
+        $aPagar = CadastroContasaPagar::findOrFail($id);
+        return view('editContasAPagar', ['aPagar' => $aPagar]);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $cadastro = CadastroContasaPagar::findOrFail($id);
+        $cadastro->fill($request->all());
+
+        $cadastro->save();
+        return redirect('/contas_a_pagar/list');
+    }
+
+    public function destroy($id)
+    {
+        CadastroContasaPagar::findOrFail($id)->delete();
+
+        return redirect('/contas_a_pagar/list')->with('msg', 'Cadastro excluido com sucesso !');
     }
 }
